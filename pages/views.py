f32c91478c8e545fad33ced5e58eec629c083663
Travis Davis <skoolboi434@gmail.com>
2024-01-27 18:02:12 -0500

    Ad Campaign template updates

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return render(request, 'pages/index.html')
+  return render(request, 'pages/campaigns-home.html')
 
