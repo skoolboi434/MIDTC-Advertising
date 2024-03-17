cad8d3fa7eec16d725471e8878fc420c0f902f2f
Travis Davis <skoolboi434@gmail.com>
2024-03-17 13:54:51 -0400

    GL Code tab updates

--- advertising/static/js/main.js
+++ advertising/static/js/main.js
@@ -231,3 +231,17 @@ $(document).ready(function () {
     e.preventDefault();
   });
 });
+
+function openSection(evt, sectionName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName('tabcontent');
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = 'none';
+  }
+  tablinks = document.getElementsByClassName('tablinks');
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(' active', '');
+  }
+  document.getElementById(sectionName).style.display = 'block';
+  evt.currentTarget.className += ' active';
+}
